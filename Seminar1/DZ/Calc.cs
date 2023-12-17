1a8350d18ce237bcb1e4f0dae24302e380529e64
Daiza <salkodg@gmail.com>
2023-12-17 18:22:39 +0500

    Finish DZ

---
+++ Seminar1/DZ/Calc.cs
@@ -0,0 +1,47 @@
+﻿namespace DZ
+{
+    public class Calc
+    {
+        public static int Calculate(string[] example)
+        {
+            int a;
+            int b;
+            
+            if (example.Length > 3 || example.Length < 3)
+            {
+                throw new ArgumentException("Некорректное количество аргументов. Допускаются три аргумента: Число > арифметический знак > число");
+            }
+
+            if (int.TryParse(example[0], out int result) && int.TryParse(example[2], out int result2))
+            {
+                a = result;
+                b = result2;
+            }
+            else
+            {
+                throw new FormatException("Вместо цифр указано, что-то другое. Укажите пример в формате: Число > арифметический знак > число");
+            }
+
+            if (example[1] == "+")
+            {
+                return a + b;
+            }
+            else if (example[1] == "-")
+            {
+                return a - b;
+            }
+            else if (example[1] == "*")
+            {
+                return a * b;
+            }
+            else if (example[1] == "/")
+            {
+                return a / b;
+            }
+            else
+            {
+                throw new ArgumentException("Некорректное значнение второго аргумента. Досукаются значения '-', '+', '*', '/'");
+            }
+        }
+    }
+}
---
+++ Seminar1/DZ/Program.cs
@@ -0,0 +1,26 @@
+﻿using static DZ.Calc;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine(Calculate(args));
+        }
+        catch (ArgumentException argEx) 
+        {
+            Console.WriteLine(argEx.Message);
+        }
+        catch (FormatException argEx) 
+        { 
+            Console.WriteLine(argEx.Message);
+        }
+        catch (DivideByZeroException argEx)
+        {
+            Console.WriteLine(argEx.Message);
+        }
+
+
+    }
+}
