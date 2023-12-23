b45320a51213d3515b39b8fb10e49b0e78d68aa5
Daiza <salkodg@gmail.com>
2023-12-23 19:25:29 +0500

    Finish task

---
+++ Seminar2/DZ/ArraySort/codeFolder/CustomSort.cs
@@ -0,0 +1,37 @@
+﻿
+namespace ArraySort.codeFolder
+{
+    public static class CustomSort
+    {
+        public static void CSort(int[,] array)
+        {
+            int counter = 0;
+
+            int[] sort = array.Cast<int>().ToArray();
+
+            Array.Sort(sort);
+            
+            for (int i = 0; i < array.GetLength(0); i++) 
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = sort[counter];
+                    counter++;
+                }
+            }
+        }
+        public static void Print2DArrayInConsole(int[,] arr)
+        {
+            int counter = 0;
+            foreach (int i in arr)
+            {
+                counter++;
+                Console.Write($"{i} ");
+                if (counter % arr.GetLength(1) == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Seminar2/DZ/ArraySort/codeFolder/Program.cs
@@ -0,0 +1,11 @@
+﻿using ArraySort.codeFolder;
+
+internal class Program
+{
+    private static void Main()
+    {
+        int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+        CustomSort.CSort(a);
+        CustomSort.Print2DArrayInConsole(a);
+    }
+}
